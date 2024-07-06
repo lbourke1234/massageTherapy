c93ba01bd3ba9a49c7d0ec3a324285da38094d5a
lbourke1234 <lbourke1234@gmail.com>
2024-07-06 10:38:14 +0100

    pricing banner image optimised

--- src/app/pages/pricing/Banner.jsx
+++ src/app/pages/pricing/Banner.jsx
@@ -1,11 +1,17 @@
+import Image from "next/image";
 import React from "react";
+import Picture from "/public/resources/Marta_Osteopath-403.jpg";
 
 const Banner = () => {
   return (
-    <div
+    <Image
       className="
-  flex flex-col h-[70vh] justify-center items-center bg-center bg-no-repeat bg-cover"
-      style={{ backgroundImage: 'url("resources/Marta_Osteopath-403.jpg")' }}
+  flex flex-col h-[70vh] justify-center items-center bg-center bg-no-repeat bg-cover object-cover"
+      height={905.797}
+      width={2560}
+      placeholder="blur"
+      src={Picture}
+      sizes="(min-width: 2700px) 2560px, calc(94.96vw + 15px)"
     />
   );
 };
