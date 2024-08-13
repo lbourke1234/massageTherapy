1285c3a2e89c5d0ce47c81078e981797a1db7db2
lbourke1234 <lbourke1234@gmail.com>
2024-08-13 11:47:36 +0100

    sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.massagetherapy.london/",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
