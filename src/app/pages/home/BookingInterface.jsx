4617dcf6dc8fea5bd746881c7de470c2bb96a6e7
lbourke1234 <lbourke1234@gmail.com>
2024-08-14 14:07:58 +0100

    booking now lazy loads

--- src/app/pages/home/BookingInterface.jsx
+++ src/app/pages/home/BookingInterface.jsx
@@ -1,28 +1,6 @@
-"use client";
-
 import React from "react";
-import { useEffect, useRef } from "react";
 
 const BookingInterface = () => {
-  //   const iframeRef = useRef(null);
-  //   useEffect(() => {
-  //     const checkIframeHeight = () => {
-  //       if (iframeRef.current && iframeRef.current.contentWindow) {
-  //         const iframeDocument = iframeRef.current.contentWindow.document;
-  //         if (iframeDocument) {
-  //           const height = iframeDocument.documentElement.scrollHeight;
-  //           iframeRef.current.style.height = `${height}px`;
-  //         }
-  //       }
-  //     };
-
-  //     const intervalId = setInterval(checkIframeHeight, 500); // Check height every 500ms
-
-  //     return () => {
-  //       clearInterval(intervalId); // Clear interval on component unmount
-  //     };
-  //   }, []);
-
   return (
     <div className="w-3/4 mx-auto py-8 h-full">
       <p id="booking-form-heading" className="text-black text-2em text-center font-medium">
@@ -37,6 +15,7 @@ const BookingInterface = () => {
         height="1160px"
         style={{ pointerEvents: "auto" }}
         className="max-w-[51.25rem] mx-auto"
+        loading="lazy"
       ></iframe>
     </div>
   );
