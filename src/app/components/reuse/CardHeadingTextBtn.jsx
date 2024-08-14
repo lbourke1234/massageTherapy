a6e7178992ff2f5595ac354cbd8d84b278258e98
lbourke1234 <lbourke1234@gmail.com>
2024-08-14 17:50:21 +0100

    services now eager loading images

--- src/app/components/reuse/CardHeadingTextBtn.jsx
+++ src/app/components/reuse/CardHeadingTextBtn.jsx
@@ -15,6 +15,7 @@ const CardHeadingTextBtn = ({ heading, src, link, alt }) => {
             height={450}
             sizes="(min-width: 800px) 450px, 200px"
             placeholder="blur"
+            loading="eager"
           />
         </div>
         <div className="card-body text-center">
