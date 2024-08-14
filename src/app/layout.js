c623eb7815bd6a694f66e85704c8b17a7c80ed35
lbourke1234 <lbourke1234@gmail.com>
2024-08-14 14:31:42 +0100

    jquery script removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,20 +28,14 @@ export default function RootLayout({ children }) {
           content="massage therapist, London, relaxation, well-being, therapy, massage services, Deep tissue, Sports, Remedial, Manual lymphatic drainage, Pregnancy, Myofascial release, Relaxing, Buccal Massage, Nutrition/Diet Advice"
         />
         <meta name="theme-color" content="#000000" />
-        <Script
+        {/* <Script
           src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
           strategy="beforeInteractive"
-        />
+        /> */}
         <Script
           src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"
           strategy="beforeInteractive"
         />
-        {/* <Script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-          crossOrigin="anonymous"
-          strategy="lazyOnload"
-        /> */}
         <Script id="cliniko-script">
           {`
           window.addEventListener('message', function handleIFrameMessage (e) {
