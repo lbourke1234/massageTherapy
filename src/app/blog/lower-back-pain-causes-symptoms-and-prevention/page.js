ff4a917e4fd88dc9bbaee2815433e688d273016f
lbourke1234 <lbourke1234@gmail.com>
2024-10-23 10:38:47 +0100

    add Lower back pain article

---
+++ src/app/blog/lower-back-pain-causes-symptoms-and-prevention/page.js
@@ -0,0 +1,14 @@
+import LowBackPain from "@/app/pages/blogPosts/LowBackPain";
+import React from "react";
+
+export const metadata = {
+  title: "Deep Tissue Massage vs. Sports Massage: Key Differences, Benefits, and Techniques",
+  description:
+    "Discover the key differences between deep tissue massage and sports massage, their unique benefits, and how each is performed. Learn which massage technique is right for your needs.",
+};
+
+const page = () => {
+  return <LowBackPain />;
+};
+
+export default page;
