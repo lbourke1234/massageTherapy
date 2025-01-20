1479ba46f65c6110e4e1d430717844e27b676b1b
lbourke1234 <lbourke1234@gmail.com>
2025-01-20 14:39:44 +0000

    cancellations copy amended

--- src/app/pages/pricing/Content.jsx
+++ src/app/pages/pricing/Content.jsx
@@ -15,8 +15,8 @@ const Content = () => {
         <p>Gift vouchers available</p>
         <h3 className="font-medium text-2xl mb-2">Payments</h3>
         <p>{`Payments: Upfront online, card or cash at the clinic.`}</p>
-        <h3 className="font-medium text-2xl mb-2">Cancellation</h3>
-        <p>{`Cancellations: 24hrs cancellation policy with full refund on all treatments.`}</p>
+        <h3 className="font-medium text-2xl mb-2">Cancellation and Rescheduling</h3>
+        <p>{`Cancellations and rescheduling: 24hrs cancellation policy with full refund on all treatments.`}</p>
         <p>
           {`Please note changes or cancellations after that time won't be possible and you will be
           charged the full amount.`}
